97541e5d5c5b169180cf7d4c1d5e663ed6334486
firu11 <firu@firu.dev>
2026-05-14 17:09:10 +0200

    fix gh pages url

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.GITHUB_PAGES == 'true' ? '/web-client/' : '/',
+  base: process.env.GITHUB_PAGES == 'true' ? '/kaj-calendar/' : '/',
 });
